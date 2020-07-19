e7c3074874242d51cd1851c0376c5401937ddf14
Sanjayso27 <sanjayso@iitk.ac.in>
2020-07-19 20:12:27 +0530

    Add aruco marker detector for the rover

---
+++ rover_aruco_detector/include/rover_aruco_detector/aruco_centre.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cv_bridge/cv_bridge.h>
+#include <geometry_msgs/Point.h>
+#include <opencv2/opencv.hpp>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+
+namespace roboiitk::CentreDetect{
+    class CentreDetect {
+        private:
+        ros::Publisher box_pub_,box_centre_pub;
+        ros::Publisher image_pub_preprocess_hsv_;
+        ros::Subscriber image_sub_;
+        cv::Mat src_,processed_frame_,result,result1,img_hsv;
+        geometry_msgs::Point center;
+        int low_H=0, low_S=0, low_V=225,high_H=255, high_S=30, high_V=255;
+        double distance_=0.0;
+
+        public:
+        void imageCb(const sensor_msgs::ImageConstPtr& msg);
+        void init(ros::NodeHandle& nh, ros::NodeHandle& nh_private);
+        void run(ros::NodeHandle& nh_private);
+        cv::Mat preprocess(cv::Mat& img);
+        void findCentre(cv::Mat& frame);
+    };
+}
